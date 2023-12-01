e02cefece5cc2cdbe5c077cc2ee4479aeba1af2f
Jared Mandap <jaredmandap@gmail.com>
2023-12-01 17:18:08 -0600

    Add Notes schema

---
+++ server/models/Notes.js
@@ -0,0 +1,25 @@
+const mongoose = require('mongoose')l
+
+const noteSchema = new mongoose.Schema({
+    title: {
+        type: String,
+        required: true,
+    },
+    content: {
+        type: String,
+        required: true,
+    },
+    userId: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
+        required: true,
+    },
+    timestamp: {
+        type: Date,
+        default: Date.now,
+    },
+});
+
+const Note = mongoose.model('Notes', noteSchema);
+
+module.exports = Note;
